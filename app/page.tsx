cbec709d1bc10828c4452cdeb49758c4f04d686c
Cameron King <cyking1233@gmail.com>
2024-07-08 05:44:19 +0000

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+import Orb from "@/components/orb";
+
+export default function Home() {
+  return (
+    <main className="flex flex-col h-full items-center justify-between p-1">
+      <div className={styles.description}>
+        <p>
+          Add VapiBlocks components to your Next.js project&nbsp;
+          <code className={styles.code}>app/page.tsx</code>
+        </p>
+        <div>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Orb by VapiBlocks
+          </a>
+        </div>
+      </div>
+      <Orb />
+    </main>
+  );
+}
---
+++ next.config.js
@@ -0,0 +1,7 @@
+const dotenv = require("dotenv");
+dotenv.config;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+module.exports = nextConfig;
