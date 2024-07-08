d8727f333e645f20a750542e38389717877ff620
Cameron King <cyking1233@gmail.com>
2024-07-08 07:11:33 +0000

    Reference Link

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         </p>
         <div>
           <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://vapiblocks.com"
             target="_blank"
             rel="noopener noreferrer"
           >
